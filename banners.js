6e37e98dd2b5a4e112a09e6cf15794a97b49c821
Ivan Levente <38502878+rotate4all@users.noreply.github.com>
2019-11-10 17:07:59 +0200

    Update banners.js

--- banners.js
+++ banners.js
@@ -2,13 +2,13 @@ function SetBanners(){
 	var randNumber = Math.floor((Math.random() * 7) + 0);
 	var banners = [];
 
-	banners[0] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="assets/images/banners/freebitcoin0.png" /></a>';
-	banners[1] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="assets/images/banners/freebitcoin1.png" /></a>';
-	banners[2] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="assets/images/banners/freebitcoin2.png" /></a>';
-	banners[3] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="assets/images/banners/freebitcoin3.png" /></a>';
-	banners[4] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="assets/images/banners/freebitcoin4.png" /></a>';
-	banners[5] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="assets/images/banners/freebitcoin5.png" /></a>';
-	banners[6] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="assets/images/banners/freebitcoin6.png" /></a>';
+	banners[0] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="freebitcoin0.png" /></a>';
+	banners[1] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="freebitcoin1.png" /></a>';
+	banners[2] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="freebitcoin2.png" /></a>';
+	banners[3] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="freebitcoin3.png" /></a>';
+	banners[4] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="freebitcoin4.png" /></a>';
+	banners[5] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="freebitcoin5.png" /></a>';
+	banners[6] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="freebitcoin6.png" /></a>';
 	
 	var currentBanner = banners[randNumber];
 
