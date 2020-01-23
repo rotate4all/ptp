56d2c5053130ff974d06e0053e751ce14dec1ffc
Ivan Levente <38502878+rotate4all@users.noreply.github.com>
2020-01-23 12:09:35 +0200

    Update banners.js

--- banners.js
+++ banners.js
@@ -5,10 +5,10 @@ function SetBanners(){
 	banners[0] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin0.png" /></a>';
 	banners[1] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin1.png" /></a>';
 	banners[2] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin2.png" /></a>';
-	banners[3] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin3.png" /></a>';
+	banners[3] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/qysjOQ8.gif" /></a>';
 	banners[4] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin4.png" /></a>';
-	banners[5] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin5.png" /></a>';
-	banners[6] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin6.png" /></a>';
+	banners[5] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/J5pA0Ml.gif" /></a>';
+	banners[6] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/8UIBouq.gif" /></a>';
 	
 	var currentBanner = banners[randNumber];
 
