45c32c10cb160ba5e3e65953be06bf9d8b825d11
Ivan Levente <38502878+rotate4all@users.noreply.github.com>
2020-06-03 10:36:35 +0300

    Update banners.js

--- banners.js
+++ banners.js
@@ -1,26 +1,29 @@
 function SetBanners(){
-	var randNumber = Math.floor((Math.random() * 17) + 0);
+	var randNumber = Math.floor((Math.random() * 20) + 0);
 	var banners = [];
 
 	banners[0] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin0.png" /></a>';
 	banners[1] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin1.png" /></a>';
 	banners[2] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin2.png" /></a>';
-	banners[3] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/qysjOQ8.gif" /></a>';
+	banners[3] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin3.png" /></a>';
 	banners[4] = '<a href="https://freebitco.in/?r=109131" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://cdn.jsdelivr.net/gh/rotate4all/ptp/freebitcoin4.png" /></a>';
-	banners[5] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/J5pA0Ml.gif" /></a>';
-	banners[6] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/8UIBouq.gif" /></a>';
-	banners[7] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/g2uzcOD.gif" /></a>';
-	banners[8] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/iVaTzik.gif" /></a>';
-	banners[9] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/CQwP1Pg.gif" /></a>';
-	banners[10] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/t0qcdLO.gif" /></a>';
-	banners[11] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/hKNNXJd.gif" /></a>';
-	banners[12] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/0e0M39U.gif" /></a>';
-	banners[13] = '<a href="https://www.stake.com/?c=d326b9ce20" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/CSTihHg.gif" /></a>';
-	banners[14] = '<a href="https://www.easyhits4u.com/splash/?ref=ryan102383" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/4JdsC0Q.gif" /></a>';
-	banners[15] = '<a href="https://www.easyhits4u.com/splash/?ref=ryan102383" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/4JdsC0Q.gif" /></a>';
-	banners[16] = '<a href="https://www.easyhits4u.com/splash/?ref=ryan102383" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/4JdsC0Q.gif" /></a>';
-	banners[17] = '<a href="https://www.easyhits4u.com/splash/?ref=ryan102383" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/4JdsC0Q.gif" /></a>';
-	banners[18] = '<a href="https://www.easyhits4u.com/splash/?ref=ryan102383" target="_blank"><img title="Earn Free Bitcoin NOW!" alt="freebitco.in" src="https://i.imgur.com/4JdsC0Q.gif" /></a>';
+	banners[5] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/HJ7J6bA.png" /></a>';
+	banners[6] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/62RBF2V.png" /></a>';
+	banners[7] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/WrConLY.png" /></a>';
+	banners[8] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/8W2PSux.png" /></a>';
+	banners[9] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/IfvbsW4.png" /></a>';
+	banners[10] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/Wki7BxG.png" /></a>';
+	banners[11] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/HJ7J6bA.png" /></a>';
+	banners[12] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/62RBF2V.png" /></a>';
+	banners[13] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/WrConLY.png" /></a>';
+	banners[14] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/8W2PSux.png" /></a>';
+	banners[15] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/IfvbsW4.png" /></a>';
+	banners[16] = '<a href="https://grammarly.go2cloud.org/aff_c?offer_id=10&aff_id=57788" target="_blank"><img title="20 million people use Grammarly to improve their writing" alt="Grammarly" src="https://i.imgur.com/Wki7BxG.png" /></a>';
+	banners[17] = '<a href="https://www.easyhits4u.com/splash/?ref=ryan102383" target="_blank"><img title="EasyHits4U - Site Promotion Made Easy" alt="EasyHits4U" src="https://i.imgur.com/7JZLour.gif" /></a>';
+	banners[18] = '<a href="https://www.easyhits4u.com/splash/?ref=ryan102383" target="_blank"><img title="EasyHits4U - Site Promotion Made Easy" alt="EasyHits4U" src="https://i.imgur.com/4JdsC0Q.gif" /></a>';
+	banners[19] = '<a href="https://www.easyhits4u.com/splash/?ref=ryan102383" target="_blank"><img title="EasyHits4U - Site Promotion Made Easy" alt="EasyHits4U" src="https://i.imgur.com/7JZLour.gif" /></a>';
+	banners[20] = '<a href="https://www.easyhits4u.com/splash/?ref=ryan102383" target="_blank"><img title="EasyHits4U - Site Promotion Made Easy" alt="EasyHits4U" src="https://i.imgur.com/4JdsC0Q.gif" /></a>';
+	banners[21] = '<a href="https://www.easyhits4u.com/splash/?ref=ryan102383" target="_blank"><img title="EasyHits4U - Site Promotion Made Easy" alt="EasyHits4U" src="https://i.imgur.com/7JZLour.gif" /></a>';
 	
 	var currentBanner = banners[randNumber];
 
